6fabb5df5a96f0595d27a4052f6a4f3195ddb22b
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-12-02 18:28:36 -0500

    Update create_requirements_txt.py
    if the dependency file only contained python packages/didnt have any lines after the last package it couldn't find the end of the packages

--- .github/workflows/create_requirements_txt.py
+++ .github/workflows/create_requirements_txt.py
@@ -6,12 +6,18 @@ with open(os.path.dirname(__file__) + '/../../dependencies.md', 'r') as f:
 python_package_idx = contents.index('## Python Packages')+1
 end_of_python_packages : int
 
+line_count = len(contents)
+
 for n, line in enumerate(contents[python_package_idx:]):
     if '#' in line:
         end_of_python_packages = n
         break
+    # its the last line in the file
+    elif n + python_package_idx + 1 == line_count:
+        end_of_python_packages = n
+        break
 
-packages = contents[python_package_idx:end_of_python_packages+python_package_idx]
+packages = contents[python_package_idx:end_of_python_packages+python_package_idx+1]
 
 with open(os.path.dirname(__file__) + '/requirements.txt', 'w') as f:
     for package in packages:
