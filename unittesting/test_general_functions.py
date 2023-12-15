3f1aa7b235afd4cc6aae06b64139745a3a7eec3d
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-12-14 22:58:19 -0500

    Added General Functions
    get_daily_returns()
    get_position_weights()

---
+++ unittesting/test_general_functions.py
@@ -0,0 +1,26 @@
+import sys
+
+# Add parent directory to path
+sys.path.append('../risk-analysis')
+
+import unittest
+from general_functions import get_daily_returns
+import pandas as pd
+import numpy as np
+
+class TestRiskFunctions(unittest.TestCase):
+    def setUp(self):
+        self.SP500_df = pd.read_csv('unittesting/_ES_Data.csv')
+
+
+    def test_daily_returns(self):
+        returns_df = get_daily_returns(self.SP500_df)
+        
+        expected_result = pd.read_csv('unittesting/_ES_Returns.csv', index_col='Date')
+
+        returns_df.equals(expected_result)
+
+
+
+if __name__ == '__main__':
+    unittest.main(failfast=True)
