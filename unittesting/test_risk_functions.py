5b68ae6c7fa81fe58201a1f2e9cd141c32a382e7
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-01-02 20:02:45 -0500

    Moved statistical_tests to separate file

--- unittesting/test_risk_functions.py
+++ unittesting/test_risk_functions.py
@@ -5,7 +5,6 @@ sys.path.append('../risk_analysis')
 
 import unittest
 from risk_functions import RiskEstimates, PositionLimits, Volatility, RiskOverlay, Margins, Periods
-import statistical_functions as StatisticalCalculations
 from statistical_functions import Periods
 import pandas as pd
 import numpy as np
@@ -14,7 +13,6 @@ import numpy as np
 class TestRiskFunctions(unittest.TestCase):
     def setUp(self):
         # Initialize any objects or variables needed for the tests
-        self.stat_calc = StatisticalCalculations
         self.risk_estimates = RiskEstimates
         self.position_limits = PositionLimits()
         self.volatility = Volatility()
@@ -51,120 +49,6 @@ class TestRiskFunctions(unittest.TestCase):
         position_weights_dct = {'SP500' : [-2.1], 'US10' : [1.68]}
 
         self.position_weights = pd.DataFrame().from_dict(position_weights_dct)
-
-
-    def test_SMA(self):
-        # Test the SMA function
-        lst = [1, 2, 3, 4, 5]
-        span = 3
-        expected_result = 3.0
-        result = self.stat_calc.SMA(lst, span)
-        self.assertEqual(result, expected_result)
-
-    def test_SR(self):
-        # Test the SR function
-        returns = [0.06, 0.08, 0.08, 0.08, 0.08, 0.09, 0.09]
-        expected_result = 8.0
-        result = self.stat_calc.SR(returns)
-        self.assertEqual(result, expected_result)
-
-    def test_EWMA(self):
-        # Test the EWMA function
-        returns = [1.00, 1.25, 1.20, 0.90]
-        expected_result = 1.0875000000000001
-        result = self.stat_calc.EWMA(returns, span=7, threshold=3)
-        self.assertAlmostEqual(result, expected_result, places=5)
-
-    def test_stddev(self):
-        # Test the stddev function
-        returns = [1, 2, 3]
-        expected_result = 1.0
-        result = self.stat_calc.std(returns)
-        self.assertEqual(result, expected_result)
-
-    def test_VAR(self):
-        # Test the VAR function
-        returns = [1, 2, 3]
-        expected_result = 1.0
-        result = self.stat_calc.VAR(returns)
-        self.assertEqual(result, expected_result)
-
-    def test_ew_stddev(self):
-        # Test the exponentialy_weighted_stddev function
-        returns = [100, 100, 100, 115, 100, 100, 115, 100, 100, 115, 100, 100, 115, 100, 100, 115, 100, 100, 115]
-        expected_result = 6.784005252999681
-        result = self.stat_calc.exponentially_weighted_stddev(returns, span=7)
-        self.assertEqual(result, expected_result)
-
-    # def test_ew_var(self):
-    #     # Test the exponentialy_weighted_var function
-    #     returns = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-    #     expected_result = 0.0
-    #     result = self.stat_calc.exponentially_weighted_var(returns, alpha=0.94)
-    #     self.assertAlmostEqual(result, expected_result)
-
-    # def test_ew_covar(self):
-    #     # Test the exponentialy_weighted_covar function
-    #     returns_X = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-    #     returns_Y = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-    #     expected_result = 0.0
-    #     result = self.stat_calc.exponentially_weighted_covar(returns_X, returns_Y, alpha=0.94)
-    #     self.assertAlmostEqual(result, expected_result)
-
-    # def test_ew_corr(self):
-    #     # Test the exponentialy_weighted_corr function
-    #     returns_dict = {'x' : [100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-    #                     'y' : [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]}
-    #     returns_df = pd.DataFrame.from_dict(returns_dict)
-
-    #     expected_result = np.array([[1.0, np.nan], [np.nan, 1.0]])
-    #     result = self.stat_calc.exponentially_weighted_correlation_matrix(returns_df, period=Periods.WEEKLY, span=7)
-    #     np.testing.assert_array_equal(result, expected_result)
-    
-    # def test_ew_portfolio_cov(self):
-    #     # Test the exponentialy_weighted_portfolio_covar function
-    #     pct_returns_dict = {'x' : [1.0, 2.0, -4.0, 5.0, .5, -2.0, -1.0, -3.0, -4.0, 5.0, .5, -2.0, -1.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 3.0, -4.0, 5.0, -2.0, -1.0],
-    #                         'y' : [3.0, -4.0, 5.0, -2.0, -1.0, 1.0, 2.0, -3.0, -4.0, 5.0, .5, -2.0, -1.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 5.0, -2.0, -1.0, 1.0, 2.0]}
-    #     pct_returns_df = pd.DataFrame.from_dict(pct_returns_dict)
-
-    #     expected_result = 0.0
-    #     result = self.stat_calc.exponentially_weighted_portfolio_covar(pct_returns_df)
-    #     self.assertEqual(result, expected_result)
-        
-    def test_carver_portfolio_correlation(self):
-        # Test using Carver's example from his book (comparing SP500 and US10)
-        expected_result = -0.22303378304662771
-
-        result = self.stat_calc.correlation(self.sp500, self.us10)
-
-        # Need this since the GitHub workflow machine is not as precise as my machine
-        self.assertAlmostEqual(result, expected_result, places=5)
-
-
-    def test_carver_portfolio_correlation_matrix(self):
-        # Test using Carver's example from his book (comparing SP500 and US10)
-        expected_result = np.array([[1.0, -0.23635242796983627], [-0.23635242796983627, 1.0]])
-
-        result = self.stat_calc.correlation_matrix(self.returns_matrix, period=Periods.WEEKLY, window=52)
-
-        np.testing.assert_array_almost_equal(result, expected_result, decimal=10)
-
-    def test_carver_portfolio_covar(self):
-        # Test using Carver's example from his book (comparing SP500 and US10)
-        expected_result = np.array([[0.0471353343383189, -0.0038835214482042125], [-0.0038835214482042125, 0.005727758363078177]])
-
-        result = self.stat_calc.portfolio_covar(self.returns_matrix)
-
-        np.testing.assert_array_almost_equal(result, expected_result, decimal=10)
-
-
-    def test_carver_portfolio_stddev(self):
-        # Test using Carver's example
-        expected_result = 0.5014329237041253
-
-        result = self.stat_calc.portfolio_stddev(self.position_weights, self.returns_matrix) 
-
-        self.assertAlmostEqual(result, expected_result)
 
     def test_estimated_portfolio_risk_multiplier(self):
         # Test using Carver's example
@@ -233,9 +117,11 @@ class TestRiskFunctions(unittest.TestCase):
         price = 97
         multiplier = 2500
 
+        notional_exposure_per_contract = price * multiplier * fx_rate
+
         number_of_contracts = scaled_forecast * capital * IDM * instrument_weight * risk_target / (average_forecast * multiplier * price * fx_rate * annual_stddev)
 
-        result = self.position_limits.maximum_position_forecast(number_of_contracts, capital, IDM, instrument_weight, risk_target, multiplier, price, fx_rate, annual_stddev, average_forecast, max_forecast=20)
+        result = self.position_limits.maximum_position_forecast(number_of_contracts, capital, IDM, instrument_weight, risk_target, notional_exposure_per_contract, annual_stddev, average_forecast, max_forecast=20)
 
         # expect the minimum of the two numbers to be the number of contracts since the max forecast is less
 
