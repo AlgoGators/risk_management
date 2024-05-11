7b6db4ac39a8518d020b26700c32b75f0c7255aa
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-05-11 15:18:34 -0400

    New tests and data

--- risk_measures/unittesting/test_risk_measures.py
+++ risk_measures/unittesting/test_risk_measures.py
@@ -17,64 +17,27 @@ class TestRiskMetrics(unittest.TestCase):
         for instrument in instruments:
             self.trend_tables[instrument] = pd.read_parquet(f'risk_measures/unittesting/data/{instrument}.parquet')
 
+        self.risk_calculations = risk_measures.RiskMeasures(self.trend_tables, (0.01, 0.01, 0.98), 100)
+        self.risk_calculations.construct()
 
-    def test_calculate_daily_returns(self):
-        df = risk_measures.calculate_daily_returns(self.trend_tables, 'Unadj_Close', 'Delivery Month', 'Date', fill=True)
-        
-        pd.testing.assert_frame_equal(df, pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet'))
+    def test_risk_calculations(self):
+        self.assertIsInstance(self.risk_calculations, risk_measures.RiskMeasures)
+        self.assertIsInstance(self.risk_calculations.daily_returns, pd.DataFrame)
+        self.assertIsInstance(self.risk_calculations.product_returns, pd.DataFrame)
+        self.assertIsInstance(self.risk_calculations.GARCH_variances, pd.DataFrame)
+        self.assertIsInstance(self.risk_calculations.GARCH_covariances, pd.DataFrame)
 
-    def test_calculate_weekly_returns(self):
-        daily_returns = pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet')
+    def test_daily_returns(self):
+        pd.testing.assert_frame_equal(self.risk_calculations.daily_returns, pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet'))
 
-        df = risk_measures.calculate_weekly_returns(daily_returns, fill=True)
-        
-        pd.testing.assert_frame_equal(df, pd.read_parquet('risk_measures/unittesting/data/weekly_returns.parquet'))
+    def test_product_returns(self):
+        pd.testing.assert_frame_equal(self.risk_calculations.product_returns, pd.read_parquet('risk_measures/unittesting/data/product_returns.parquet'))
 
-    def test_calculate_product_returns(self):
-        weekly_returns = pd.read_parquet('risk_measures/unittesting/data/weekly_returns.parquet')
+    def test_GARCH_variances(self):
+        pd.testing.assert_frame_equal(self.risk_calculations.GARCH_variances, pd.read_parquet('risk_measures/unittesting/data/GARCH_variances.parquet'))
 
-        df = risk_measures.calculate_product_returns(weekly_returns, fill=True)
-
-        pd.testing.assert_frame_equal(df, pd.read_parquet('risk_measures/unittesting/data/product_returns.parquet'))
-
-    def test_update_product_returns(self):
-        all_product_returns = pd.read_parquet('risk_measures/unittesting/data/product_returns.parquet')
-        product_returns = all_product_returns.iloc[:-1, :]
-        weekly_returns = pd.read_parquet('risk_measures/unittesting/data/weekly_returns.parquet').iloc[[-1]]
-
-        product_returns : pd.DataFrame = risk_measures.update_product_returns(product_returns, weekly_returns)
-        pd.testing.assert_frame_equal(product_returns, all_product_returns)
-
-    def test_calculate_GARCH_variances(self):
-        daily_returns = pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet')
-
-        df = risk_measures.calculate_GARCH_variances(daily_returns, 100, (0.01, 0.01, 0.98), fill=True)
-        
-        pd.testing.assert_frame_equal(df, pd.read_parquet('risk_measures/unittesting/data/GARCH_variances.parquet'))
-
-    def test_update_GARCH_variance(self):
-        all_variances = pd.read_parquet('risk_measures/unittesting/data/GARCH_variances.parquet')
-        variances = all_variances.iloc[:-1, :] # .iloc[:-1] to simulate not having the last estimate
-        returns = pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet')
-
-        variances : pd.DataFrame = risk_measures.update_GARCH_variance(variances, returns, 100, (0.01, 0.01, 0.98))
-        pd.testing.assert_series_equal(variances.iloc[-1], all_variances.iloc[-1])
-
-    def test_calculate_GARCH_covariances(self):
-        product_returns = pd.read_parquet('risk_measures/unittesting/data/product_returns.parquet')
-        expected_df = pd.read_parquet('risk_measures/unittesting/data/GARCH_covariances.parquet')
-
-        df = risk_measures.calculate_GARCH_covariances(product_returns, 100, (0.01, 0.01, 0.98), fill=True)
-
-        pd.testing.assert_frame_equal(df, expected_df)
-
-    def test_update_GARCH_covariance(self):
-        all_covariances = pd.read_parquet('risk_measures/unittesting/data/GARCH_covariances.parquet')
-        covariances = all_covariances.iloc[:-1, :]
-        returns = pd.read_parquet('risk_measures/unittesting/data/product_returns.parquet')
-
-        covariances : pd.DataFrame = risk_measures.update_GARCH_covariance(covariances, returns, 100, (0.01, 0.01, 0.98))
-        pd.testing.assert_series_equal(covariances.iloc[-1], all_covariances.iloc[-1])
+    def test_GARCH_covariances(self):
+        pd.testing.assert_frame_equal(self.risk_calculations.GARCH_covariances, pd.read_parquet('risk_measures/unittesting/data/GARCH_covariances.parquet'))
 
     def test_calculate_value_at_risk_historical(self):
         daily_returns = pd.read_parquet('risk_measures/unittesting/data/daily_returns.parquet')
